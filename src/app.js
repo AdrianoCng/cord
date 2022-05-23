0b56d31b68dcb6cf1f7ce66773d8c78b2f554ff8
Adriano Cangiamila <adrianocangiamila@gmail.com>
2022-05-23 22:54:54 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import styled from 'styled-components';
+
+import SideNavBar from "./components/sidenavbar";
+
+import Discover from "./pages/discover";
+
+import * as sizes from "./breakpoints";
+
+import './css/app.scss';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <PageContainer>
+          <SideNavBar {...this.props} />
+          <ContentWrapper>
+            <Switch>
+              <Route path="/discover" component={Discover} {...this.props} />
+            </Switch>
+          </ContentWrapper>
+        </PageContainer>
+      </Router>
+    );
+  }
+}
+
+
+const ContentWrapper = styled.main`
+  padding-left: 280px;
+
+  @media (max-width: ${sizes.tablet}) {
+    padding-left: 0;
+  }
+`
+
+const PageContainer = styled.main`
+  overflow-x: hidden;
+  min-width: 348px;
+`
---
+++ src/components/hamburger/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from 'styled-components';
+
+import * as sizes from "../../breakpoints";
+
+export default function Hamburger({ toogleSidebar }) {
+    return (
+        <Wrapper onClick={toogleSidebar}>
+            <Line />
+            <Line />
+            <Line />
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.div`
+    display: block;
+    height: 26px;
+    width: 32px;
+    position: absolute;
+    top: 35px;
+    left: calc(280px + 35px);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    cursor: pointer;
+
+    @media (min-width: ${sizes.tablet}) {
+        display: none;
+    }
+`
+
+const Line = styled.span`
+    display: block;
+    height: 4px;
+    width: 100%;
+    border-radius: 10px;
+    background: black;
+`
